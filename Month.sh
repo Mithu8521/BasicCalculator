baf860569973037c6aace59fc8f38509fc7da79e
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-02 23:40:42 +0530

    Find Month

---
+++ Month.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+read -p "Enter date:-" date
+read -p "Enter month:-" month
+if(( ($month <= 6 & $date <=20) ))
+then
+	echo $month $date "True";
+elif(( ($month >= 3 & $month < 6) & ($date <31) ))
+then
+	echo $date $month "True";
+else
+	echo "False";
+fi
