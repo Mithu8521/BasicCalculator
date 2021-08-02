900462c0b8f9e4a5e861731b41bad6b972f3edee
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-02 22:16:19 +0530

    Find Number In Word

---
+++ Single2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+a=$(($RANDOM%10))
+case "$a" in
+   "0") echo "Zero"
+   ;;
+   "1") echo "One" 
+   ;;
+   "2") echo "Two"
+   ;;
+   "3") echo "Three"
+   ;;
+   "4") echo "four"
+   ;;
+   "5") echo "five"
+   ;;
+   "6") echo "six"
+   ;;
+   "7") echo "seven"
+   ;;
+   "8") echo "eight"
+   ;;
+   "9") echo "Nine"
+   ;;
+
+ "1") echo "One"
+   ;;
+ "1") echo "One"
+   ;;
+ "1") echo "One"
+   ;;
+esac
+
+
+
+
